19aa2cb282b0a195ba238f7c1a876cb8572e0e22
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-06-16 18:42:25 +0800

    created timer function inside main and updated pomodoro.py

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from pomodoro import Timer
+import sys
 
-timer = Timer(1, 5)
+def timer(mins):
+    timer = Timer(mins)
+    timer.start()
+    tick_gen = timer.tick()
 
-timer.start()
-for tick in timer.tick():
-    print(f"    {tick}", end="\r")
+    print("   Ctrl+c to PAUSE\n")
+    print(f"   Start:    {timer.started.strftime('%I:%M %p')}")
+    print(f"   Est. end: {(timer.started + timer.duration).strftime('%I:%M %p')}")
+    print(f"   {'-'*18}")
+    while timer.ticking:
+        try:
+            tick = next(tick_gen)
+            print(f"   {tick}", end="\r", flush=True)
+        except KeyboardInterrupt:
+            timer.pause()
+            print(f"\r   {tick} [PAUSED] Enter to resume or Ctrl+c to quit", end="\r", flush=True)
+            try:
+                input()
+                print("\033[A\033[K", end="")
+                timer.start()
+                tick_gen = timer.tick()
+            except KeyboardInterrupt:
+                print("\r   [ABANDONED]", "  "*20)
+                sys.exit()
 
+        except StopIteration:
+            break
 
-print("\ndone")
+    print("   00:00 [DONE]")
+
+def main():
+    timer(25)
+
+if __name__ == "__main__":
+    main()
--- pomodoro.py
+++ pomodoro.py
@@ -2,32 +2,32 @@ from datetime import date, datetime, timedelta
 from time import sleep
 
 class Timer:
-    def __init__(self, duration, break_time):
-        duration = timedelta(seconds = duration * 60)
-        break_time = timedelta(seconds = break_time * 60)
-
+    def __init__(self, duration):
         self.started = datetime.now()
-        self.duration = int(duration.total_seconds())
-        self.break_time = int(break_time.total_seconds())
+        self.duration = timedelta(seconds = duration * 60)
+
         self.running = False
+        self.ticking = False
 
     def start(self):
         self.running = True
+        self.ticking = True
 
     def tick(self):
-        while self.duration > 0:
-            mins, secs = divmod(self.duration, 60)
+        while self.duration > timedelta(seconds=0):
+            mins, secs = divmod(int(self.duration.seconds), 60)
 
-            if self.running:
-                yield(f"{mins:02d}:{secs:02d}")
-                sleep(1)
-                self.duration -= 1
+            yield(f"{mins:02d}:{secs:02d}")
+            sleep(1)
 
-            else:
-                yield(f"{mins:02d}:{secs:02d}")
-                sleep(1)
-                yield("_    ")
-                sleep(1)
+            if not self.running:
+                continue
+            
+            self.duration -= timedelta(seconds=1)
+
+        yield "00:00"
+        self.ticking = False
+        self.running = False
 
     def pause(self):
         self.running = False
