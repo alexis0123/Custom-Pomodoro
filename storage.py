dd529190b9e24df27a7ba394b5ca5a7e6546fc25
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-06-18 14:29:34 +0800

    created add_task method

--- storage.py
+++ storage.py
@@ -1,16 +1,19 @@
+from datetime import date
 import json
 import os
+
 class Data:
     def __init__(self):
 
         self.path = "journal"
         self.file_path = os.path.join(self.path, "log.json")
+        self.date_today = date.today().strftime("%d/%m/%Y")
 
         os.makedirs(self.path, exist_ok=True)
 
         if not os.path.exists(self.file_path):
             with open(self.file_path, "w") as f:
-                json.dump("{}", f)
+                json.dump({}, f)
 
         self.logs = self.load()
 
@@ -21,3 +24,9 @@ class Data:
     def save(self):
         with open(self.file_path, "w") as f:
             json.dump(self.logs, f , indent=2)
+
+    def add_task(self, task):
+        if self.date_today not in self.logs:
+            self.logs[self.date_today] = {}
+
+        self.logs[self.date_today][task] = {}
