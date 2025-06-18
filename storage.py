6376cd598b896eda7c3d23c3da04b50eee6222c6
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-06-18 10:12:02 +0800

    refactored storage module and added journal folder to gitignore

--- storage.py
+++ storage.py
@@ -1,14 +1,13 @@
 import json
 import os
+class Data:
+    def __init__(self):
 
-path = "journal"
-file_path = os.path.join(path, "log.json")
+        self.path = "journal"
+        self.file_path = os.path.join(self.path, "log.json")
 
-try:
-    os.makedirs(path)
-except FileExistsError:
-    pass
+        os.makedirs(self.path, exist_ok=True)
 
-if not os.path.exists(file_path):
-    with open(file_path, "w") as f:
-        pass
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, "w") as f:
+                json.dump("{}", f)
