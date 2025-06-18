1f8f22ab8ef7cb04d43744b7ce4a225c7ed20e1a
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-06-18 20:09:26 +0800

    added get_tasks method that returns list of task and number of pomos inside dict

--- storage.py
+++ storage.py
@@ -32,7 +32,7 @@ class Data:
         task_name = task
 
         if task_name not in self.logs[self.date_today]:
-            self.logs[self.date_today][task] = {"pomos": []}
+            self.logs[self.date_today][task_name] = {"pomos": []}
 
         else:
             n = 1
@@ -54,3 +54,10 @@ class Data:
             raise ValueError(f"{task_name} doesnt exist.. you must add_task() first")
         
         self.logs[self.date_today][task_name]["reflection"] = reflection
+
+    def get_tasks(self) -> list:
+        tasks = []
+        for task in self.logs[self.date_today]:
+            tasks.append({"task": task, "pomos": len(self.logs[self.date_today][task]["pomos"])})
+
+        return tasks
